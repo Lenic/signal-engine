c1c96d402d5aa91bb16e5d3cec8d4044295321d8
Lenic Lei <lenic_lei@manka.co.jp>
2026-09-03 17:52:03 +0900

    remove original implementation core.
    Signed-off-by: Lenic Lei <lenic_lei@manka.co.jp>

--- src/core/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './connector-manager';
-export * from './dirty-markable';
-export * from './schedulable';
-export * from './scheduler';
-export * from './types';
-export * from './version-follower';
-export * from './version-leader';
